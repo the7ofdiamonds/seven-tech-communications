d60a6b0050accf2fc6e2103bdf8a675ba14dd123
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-04-05 17:46:47 -0400

    Refactoring 4/5/24 @ 17:46

--- API/Gateway.php
+++ API/Gateway.php
@@ -6,23 +6,47 @@ use Exception;
 
 use WP_REST_Request;
 
+use SEVEN_TECH\Communications\Email\Gateway\EmailGateway;
+use SEVEN_TECH\Communications\Email\Gateway\EmailGatewayLink;
+
 use PHPMailer\PHPMailer\PHPMailer;
 
 class Gateway
 {
     private $mailer;
+    private $emailGateway;
+    private $emailGatewayLink;
+    private $websiteName;
+    private $deletionTime;
+    private $confirmationCodeExpiration;
 
     public function __construct(PHPMailer $mailer)
     {
         $this->mailer = $mailer;
+        $this->emailGateway = new EmailGateway($this->mailer);
+        $this->emailGatewayLink = new EmailGatewayLink($this->mailer);
+        $this->websiteName = 'THE7OFDIAMONDS.TECH';
+        $this->deletionTime = '90 Days';
+        $this->confirmationCodeExpiration = '15 minutes';
     }
 
     public function signup(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send signup email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = 'Signup Email';
+
+            $message = "Thanks for joining {$this->websiteName}  {$user->name}";
+
+            $signupEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($signupEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -38,9 +62,29 @@ class Gateway
     public function verifyAccount(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send verify account email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $link = $request['link'];
+
+            if (empty($link)) {
+                $statusCode = 400;
+                throw new Exception('Link is required to send verify account email.', $statusCode);
+            }
+
+            $buttonName = 'VERIFY';
+
+            $subject = 'Account Verification';
+
+            $message = "Click the link below to verify your account at {$this->websiteName}.";
+
+            $verifyAccountEmailResponse = $this->emailGatewayLink->sendGatewayEmail($user, $subject, $message, $link, $buttonName);
+
+            return rest_ensure_response($verifyAccountEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -56,9 +100,20 @@ class Gateway
     public function verifiedAccount(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send verified account email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = 'Account Verfified';
+
+            $message = "Thank you {$user->name}, your account has been verified at {$this->websiteName}.";
+
+            $verifiedAccountEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($verifiedAccountEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -74,9 +129,29 @@ class Gateway
     public function lockedAccount(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send account locked email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $link = $request['link'];
+
+            if (empty($link)) {
+                $statusCode = 400;
+                throw new Exception('Link is required to send account locked email.', $statusCode);
+            }
+
+            $buttonName = 'UNLOCK';
+
+            $subject = "Your account is locked at {$this->websiteName}";
+
+            $message = "At this time your account is locked at {$this->websiteName}. Click the link below to restore your access and permissions";
+
+            $lockedAccountEmailResponse = $this->emailGatewayLink->sendGatewayEmail($user, $subject, $message, $link, $buttonName);
+
+            return rest_ensure_response($lockedAccountEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -92,9 +167,29 @@ class Gateway
     public function removedAccount(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send account removed email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $link = $request['link'];
+
+            if (empty($link)) {
+                $statusCode = 400;
+                throw new Exception('Link is required to send account removed email.', $statusCode);
+            }
+
+            $buttonName = 'RESTORE';
+
+            $subject = "Your account has been removed at {$this->websiteName}";
+
+            $message = "At this time your account has been removed at {$this->websiteName}. You have {$this->deletionTime} to restore this account before it is deleted.";
+
+            $removedAccountEmailResponse = $this->emailGatewayLink->sendGatewayEmail($user, $subject, $message, $link, $buttonName);
+
+            return rest_ensure_response($removedAccountEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -110,9 +205,20 @@ class Gateway
     public function deletedAccount(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send account deleted email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = "Your account has been deleted at {$this->websiteName}";
+
+            $message = "At this time your account has been deleted at {$this->websiteName}.";
+
+            $deletedAccountEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($deletedAccountEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -128,9 +234,29 @@ class Gateway
     public function forgotPassword(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send forgot password email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $link = $request['link'];
+
+            if (empty($link)) {
+                $statusCode = 400;
+                throw new Exception('Link is required to send forgot password email.', $statusCode);
+            }
+
+            $buttonName = 'CHANGE';
+
+            $subject = "Change Password at {$this->websiteName}";
+
+            $message = "Below is a link to change your password. You have {$this->confirmationCodeExpiration} to use this email to change your password.";
+
+            $forgotPasswordEmailResponse = $this->emailGatewayLink->sendGatewayEmail($user, $subject, $message, $link, $buttonName);
+
+            return rest_ensure_response($forgotPasswordEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -146,9 +272,29 @@ class Gateway
     public function updatePassword(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send update password email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $link = $request['link'];
+
+            if (empty($link)) {
+                $statusCode = 400;
+                throw new Exception('Link is required to send update password email.', $statusCode);
+            }
+
+            $buttonName = 'UPDATE';
+
+            $subject = "Update Password at {$this->websiteName}";
+
+            $message = "Below is a link to update your password. You have {$this->confirmationCodeExpiration} to use this email to update your password.";
+
+            $updatePasswordEmailResponse = $this->emailGatewayLink->sendGatewayEmail($user, $subject, $message, $link, $buttonName);
+
+            return rest_ensure_response($updatePasswordEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -164,9 +310,20 @@ class Gateway
     public function changedPassword(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send changed password email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = "Password Changed at {$this->websiteName}";
+
+            $message = "At this time your password has been changed at {$this->websiteName}.";
+
+            $changedPasswordEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($changedPasswordEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -182,9 +339,20 @@ class Gateway
     public function changedName(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send changed name email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = "Your name has been changed at {$this->websiteName}";
+
+            $message = "At this time your name was changed at {$this->websiteName}.";
+
+            $changedNameEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($changedNameEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -200,9 +368,20 @@ class Gateway
     public function changedPhone(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send changed phone email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = "Your phone number was changed at {$this->websiteName}";
+
+            $message = "At this time your phone number was changed at {$this->websiteName}.";
+
+            $changedPhoneEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($changedPhoneEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
@@ -218,9 +397,20 @@ class Gateway
     public function changedUsername(WP_REST_Request $request)
     {
         try {
+            $user = $request['user'];
 
+            if (empty($user)) {
+                $statusCode = 400;
+                throw new Exception('User is required to send changed username email.', $statusCode);
+            }
 
-            return rest_ensure_response("");
+            $subject = "Your username was changed at {$this->websiteName}";
+
+            $message = "At this time your username was changed at {$this->websiteName}.";
+
+            $changedUsernameEmailResponse = $this->emailGateway->sendGatewayEmail($user, $subject, $message);
+
+            return rest_ensure_response($changedUsernameEmailResponse);
         } catch (Exception $e) {
             $message = array(
                 'errorMessage' => $e->getMessage(),
